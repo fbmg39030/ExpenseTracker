1c511713608ac49349132a7a676ba61ecd3ad40a
Fabian Moling <fabian.mf11@gmail.com>
2023-12-23 13:16:13 +1100

    Add Shop Unit Test

---
+++ Shop.Test/Persistence/ProductTest.cs
@@ -0,0 +1,34 @@
+﻿using Shop.API.Models;
+using Shop.API.Persistence.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Test.Persistence
+{
+    [TestClass]
+    public class ProductTest
+    {
+        //CRUD --> CREATE; READ; UPDATE; DELETE
+        [TestMethod]
+        public void CreateProduct()
+        {
+            var productLoid = Guid.NewGuid();
+            var productDbo = new ProductDbo
+            {
+                LogicalObjectId = productLoid,
+                Name1 = "Name_" + productLoid,
+                Description = "Description_" + productLoid,
+                Price = 20.2m
+            };
+
+            ProductDao.SaveOrUpdate(productDbo);
+
+            var queriedProductDbo = ProductDao.QueryByLogicalObjectId(productLoid);
+            Assert.IsNotNull(queriedProductDbo);
+
+        }
+    }
+}
