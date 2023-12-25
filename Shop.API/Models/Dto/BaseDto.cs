e3de59f6e17020b7ef4f50ffe995a601657c832e
Fabian Moling <fabian.mf11@gmail.com>
2023-12-25 13:30:45 +1100

    Add base and product dtos

---
+++ Shop.API/Models/Dto/BaseDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Shop.API.Models.Dbo;
+
+namespace Shop.API.Models.Dto;
+
+public abstract class BaseDto<TDbo, TDto> where TDbo : BaseDbo where TDto : BaseDto<TDbo, TDto>
+{
+    protected BaseDto(TDbo dbo)
+    {
+        LogicalObjectId = dbo.LogicalObjectId;
+        CreatedAt = dbo.CreatedAt;
+        LastModifiedAt = dbo.LastModifiedAt;
+    }
+    public Guid LogicalObjectId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastModifiedAt { get; set; }
+}
---
+++ Shop.API/Models/Dto/ProductDto.cs
@@ -0,0 +1,37 @@
+﻿using Shop.API.Models.Dbo;
+
+namespace Shop.API.Models.Dto;
+
+public class ProductDto : BaseDto<ProductDbo, ProductDto>
+{
+    public ProductDto(ProductDbo dbo) : base(dbo)
+    {
+        
+    }
+    public string Name1 { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    //public virtual int StockQuantity { get; set; }
+    //public List<string> ImageUrl { get; set; }
+
+    public static ProductDto FromDbo(ProductDbo dbo, Dictionary<Guid, object> dtoReferences)
+    {
+        if (dtoReferences.ContainsKey(dbo.LogicalObjectId))
+        {
+            return dtoReferences[dbo.LogicalObjectId] as ProductDto;
+        }
+        var dto = new ProductDto(dbo)
+        {
+            Name1 = dbo.Name1,
+            Description = dbo.Description,
+            Price = dbo.Price,
+        };
+        dtoReferences.Add(dto.LogicalObjectId, dto);
+        return dto;
+    }
+
+    public static List<ProductDto> FromDboList(IEnumerable<ProductDbo> dbos, Dictionary<Guid, object> dtoReferences)
+    {
+        return dbos?.Select(dbo => FromDbo(dbo, dtoReferences)).ToList();
+    }
+}
