9d51e36d5f80155ea9dc0861815293d6604913df
Fabian Moling <fabian.mf11@gmail.com>
2023-12-27 15:30:07 +1100

    Add create order unit test

---
+++ Shop.Test/API/OrderApiTest.cs
@@ -0,0 +1,69 @@
+﻿using Shop.API.Api;
+using Shop.API.Models.Dbo;
+using Shop.API.Models.Dto;
+using Shop.API.Models.Request;
+using Shop.API.Persistence.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Test.API;
+
+[TestClass]
+public class OrderApiTest
+{
+    [TestMethod]
+    public void CreateOrder()
+    {
+        var productLoid = Guid.NewGuid();
+        var productDbo = new ProductDbo()
+        {
+            LogicalObjectId = productLoid,
+            Name1 = "Product1" + productLoid,
+            Price = 20m
+        };
+        ProductDao.SaveOrUpdate(productDbo);
+
+        var productLoid2 = Guid.NewGuid();
+        var productDbo2 = new ProductDbo()
+        {
+            LogicalObjectId = productLoid2,
+            Name1 = "Product2" + productLoid2,
+            Price = 10m
+        };
+        ProductDao.SaveOrUpdate(productDbo2);
+
+
+        var positionsList = new List<CreateOrderPositionRequest>();
+
+        var position1 = new CreateOrderPositionRequest()
+        {
+            Quantity = 2,
+            UnitPrice = 10,
+            ProductLoid = productLoid
+        };
+        var position2 = new CreateOrderPositionRequest()
+        {
+            Quantity = 5,
+            UnitPrice = 5m,
+            ProductLoid = productLoid2
+        };
+
+        positionsList.Add(position1);
+        positionsList.Add(position2);
+
+        OrderCreateOrUpdateRequest request = new OrderCreateOrUpdateRequest()
+        {
+            OderPositionRequests = positionsList
+        };
+        
+        var orderResult = OrderApi.CreateOrUpdate(request);
+        var orderDto = orderResult.GetType().GetProperty("Value")?.GetValue(orderResult, null) as OrderDto;
+        Assert.IsNotNull(orderDto);
+        Assert.IsTrue(orderDto.OrderPositionList.Count == 2);
+        Assert.IsTrue(orderDto.OrderPositionList[1].Quantity == 5);
+    }
+}
+
