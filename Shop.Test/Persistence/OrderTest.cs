0907ad57d74668999a96c0f210bf16137c325e89
Fabian Moling <fabian.mf11@gmail.com>
2023-12-27 15:04:51 +1100

    Update failing unit tests

--- Shop.Test/Persistence/OrderTest.cs
+++ Shop.Test/Persistence/OrderTest.cs
@@ -50,4 +50,63 @@ public class OrderTest
         Assert.IsNotNull(queriedOrderDbo);
         Assert.AreEqual(queriedOrderDbo.UserId, "TestLoid2" + loid);
     }
+
+    [TestMethod]
+    public void CreateOrderWithOrderPositions()
+    {
+        var productLoid = Guid.NewGuid();
+        var productDbo = new ProductDbo
+        {
+            LogicalObjectId = productLoid,
+            Name1 = "Bottle"+ productLoid,
+            Price = 20.5m
+        };
+        ProductDao.SaveOrUpdate(productDbo);
+
+        var productLoid2 = Guid.NewGuid();
+        var productDbo2 = new ProductDbo
+        {
+            LogicalObjectId = productLoid2,
+            Name1 = "Shoes" + productLoid2,
+            Price = 40.5m
+        };
+        ProductDao.SaveOrUpdate(productDbo2);
+
+        var orderPositionList = new List<OrderPositionDbo>();
+
+        var orderPositionsLoid = Guid.NewGuid();
+        var orderPositionsLoid2 = Guid.NewGuid();
+        var orderPosition1 = new OrderPositionDbo
+        {
+            LogicalObjectId = orderPositionsLoid,
+            Product = productDbo,
+            Quantity = 2,
+            UnitPrice = 18.5m
+        };
+        var orderPosition2 = new OrderPositionDbo
+        {
+            LogicalObjectId = orderPositionsLoid2,
+            Product = productDbo2,
+            Quantity = 2,
+            UnitPrice = 30.5m
+        };
+        orderPositionList.Add(orderPosition1);
+        orderPositionList.Add(orderPosition2);
+
+        var loid = Guid.NewGuid();
+        var orderDbo = new OrderDbo
+        {
+            LogicalObjectId = loid,
+            UserId = "TestLoid" + loid,
+            TotalAmount = 0,
+            OrderPositionList = orderPositionList
+        };
+        OrderDao.SaveOrUpdate(orderDbo);
+
+        var queriedOrderDbo = OrderDao.QueryByLogicalObjectId(orderDbo.LogicalObjectId);
+        Assert.IsNotNull(queriedOrderDbo);
+        Assert.IsTrue(queriedOrderDbo.OrderPositionList.Count == 2);
+        Assert.AreEqual(queriedOrderDbo.OrderPositionList[0].Product.LogicalObjectId, productLoid);
+
+    }
 }
